d07acceeac5abb1084639f472e54a6e21cbc019c
Laydle <117215581+aidanlbyrne@users.noreply.github.com>
2023-05-09 06:59:13 -0400

    Add files via upload
    this is the fastest valid anagram that i came up with

--- validAnagram.go
+++ validAnagram.go
@@ -1,24 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	s1 := make([]int, 26)
+	s2 := make([]int, 26)
+
+	for i, v := range s {
+		s1[v%26]++
+		s2[t[i]%26]++
+	}
+	if reflect.DeepEqual(s1, s2) {
+		return true
+	}
+	return false
+}
 
 func main() {
 	s, t := "anagram", "nagaram"
+	result := isAnagram(s, t)
 
-	s1 := make([]int, 122)
-	s2 := make([]int, 122)
-
-	for _,v := range s {
-		s1[v]++
-	}
-	for _,v := range t {
-		s2[v]++
-	}
-
-	if s1 == s2 {
-		fmt.Println("you")
-	}
-
-	
-	
 }
+// func main() {
+// 	s, t := "anagram", "nagaram"
+
+// 	s1 := make([]int, 122)
+// 	s2 := make([]int, 122)
+
+// 	for _,v := range s {
+// 		s1[v]++
+// 	}
+// 	for _,v := range t {
+// 		s2[v]++
+// 	}
+
+// 	if s1 == s2 {
+// 		fmt.Println("you")
+// 	}
+// }
